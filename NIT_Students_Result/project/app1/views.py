e5e44cdddc3f361d46ccb06f6edf40c04948e5bb
Gaurav <gauravnehare10@gmail.com>
2024-08-09 14:55:22 +0530

    upadate

--- NIT_Students_Result/project/app1/views.py
+++ NIT_Students_Result/project/app1/views.py
@@ -56,16 +56,16 @@ def find_view(request):
     except:
         msg = "Invalid Roll No"
         return render(request, 'app1/search.html', context={'msg': msg})
-    
+
+
 def find_result(request):
     marks_qset = Marks.objects.all()
     marks_list = []
     for stud in marks_qset:
-        row = [stud.rollno, stud.name, stud.subject1, stud.subject2, stud.subject3]
-        row.append(stud.subject1 + stud.subject2 + stud.subject3)
-        rs = 'PASS' if stud.subject1>=40 and stud.subject2 >= 40 and stud.subject3 >= 40 else 'FAIL'
+        row = [stud.rollno, stud.name, stud.python, stud.html, stud.javascript]
+        row.append(stud.python + stud.html + stud.javascript)
+        rs = 'PASS' if stud.python >= 40 and stud.html >= 40 and stud.javascript >= 40 else 'FAIL'
         row.append(rs)
         marks_list.append(row)
-    
-    return render(request, 'app1/result.html', context={'marks_list': marks_list})
 
+    return render(request, 'app1/result.html', context={'marks_list': marks_list})
